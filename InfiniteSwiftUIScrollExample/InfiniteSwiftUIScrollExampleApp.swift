e7929d829d28b6dc893d85677a3f2bfcc0639d69
ANSCoder <nimjea@gmail.com>
2022-06-19 17:28:26 +0530

    Initial Commit

---
+++ InfiniteSwiftUIScrollExample/InfiniteSwiftUIScrollExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteSwiftUIScrollExampleApp.swift
+//  InfiniteSwiftUIScrollExample
+//
+//  Created by Anand Nimje on 19/06/22.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteSwiftUIScrollExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
